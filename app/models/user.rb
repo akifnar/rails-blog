de346a9d2696cf54f957790c46a3fbbc9ed39be8
akifnar <akifnar@gmail.com>
2026-07-07 01:49:26 +0300

    Update tests and add passwords attribute to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   before_save { email.downcase! }
+  has_secure_password
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
-  has_secure_passwo
+  validates :password,  presence: true, length: { minimum: 8 }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Akif Nar", email: "akif@exa.com")
+    @user = User.new(name: "Akif Nar", email: "akif@exa.com",
+                      password: "foobarbaz", password_confirmation: "foobarbaz")
   end
 
   test "should be valid" do
@@ -57,5 +58,15 @@ class UserTest < ActiveSupport::TestCase
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert @user.invalid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 7
+    assert @user.invalid?
   end
 end
