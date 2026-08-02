41b7a11f05d1f78f4277847b9b3bb5fe4d5b2c74
akifnar <akifnar@gmail.com>
2026-08-02 19:25:32 +0300

    Added user mailer for activation and password reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -14,7 +14,7 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.password_reset.subject
   #
-  def password_reset
+  def password_resets
     @greeting = "Hi"
 
     mail to: "to@example.org"
